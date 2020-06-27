49f78256db0e7310e053c8509ddc32ff980e370f
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-06-27 23:48:49 +0530

    compile ts file

--- types.js
+++ types.js
@@ -1,7 +1,5 @@
-const name = "Ravindu";
-
-if(true){
-    const name = 12;
-    console.log(name);
-}
-console.log(name);
+var people = /** @class */ (function () {
+    function people() {
+    }
+    return people;
+}());
